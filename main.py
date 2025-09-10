210e88c02b897df3115f8a3ac8ed47fc87fa8d02
JonaSavage <jonathan.stickle@gmail.com>
2025-09-10 12:23:09 -0700

    Completed 3-1 Printing a report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, char_count
+from stats import *
 
 def get_book_text(path_to_book):
     with open(path_to_book) as b:
@@ -10,9 +10,17 @@ def get_book_text(path_to_book):
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = word_count(text)
-    #print(text)
-    print(f"{num_words} words found in the document")
-    print(char_count(text))
+    sorted_dictionary = sort_dics(char_count(text))
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in range(0, len(sorted_dictionary)):
+        if sorted_dictionary[i]["char"].isalpha():
+            print(f"{sorted_dictionary[i]["char"]}: {sorted_dictionary[i]["num"]}")
+    print("============= END ===============")
     
     
 main()
--- stats.py
+++ stats.py
@@ -2,6 +2,9 @@ def word_count(book_text):
     count = len(book_text.split())
     return count
 
+def sort_on(items):
+    return items["num"]
+
 
 def char_count(book_text):
     to_lower = book_text.lower()
@@ -13,3 +16,15 @@ def char_count(book_text):
             char_dict[c] += 1
     
     return char_dict
+
+
+def sort_dics(book_dic):
+    stats_list = []
+    temp_dic = {}
+    
+    for c in book_dic:
+        temp_dic = {"char": c, "num": book_dic[c]}
+        stats_list.append(temp_dic)
+        
+    stats_list.sort(reverse=True, key=sort_on)
+    return stats_list      
